a9459249ad8c407650e565e9bb4690c1a188f4ad
Mack Gallagher <wingsonthebus@gmail.com>
2022-04-21 16:45:18 -0500

    establish first styled component

--- src/components/Caption.js
+++ src/components/Caption.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components';
+
+const StyledCaption = styled.div`
+  width: 80%;
+  background-color: rgba(4,10,17,10%);
+  border: 1px solid cyan;
+  border-radius: 4px;
+`
 
 function Caption(props) {
   const { title, explanation } = props;
 
   return (
-    <div className='caption'>
+    <StyledCaption>
       <h4>{title}</h4>
       <p>{explanation}</p>
-    </div>
+    </StyledCaption>
   )
 
 }
