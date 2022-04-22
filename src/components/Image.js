d78abfbab4d5767d109a5fdfb32cd00e38241bad
Mack Gallagher <wingsonthebus@gmail.com>
2022-04-21 19:26:22 -0500

    add animation for image

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,10 +2,15 @@ import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 
 const StyledImage = styled.img`
+    transition: ease 0.2s;
     width: 80%;
     margin: 10% auto;
     border: 1px solid ${props => props.theme.accentColor};
     border-radius: 10px;
+    
+    :hover {
+      transform: scale(1.2);
+    }
 `
 
 function Image(props) {
